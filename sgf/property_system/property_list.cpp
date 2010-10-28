e7a712815540cda763352163b567a9dbc6a3db51
acidchris <developer_chris@hotmail.com>
2010-10-28 22:00:17 +0200

    initial commit

--- sgf/property_system/property_list.cpp
+++ sgf/property_system/property_list.cpp
@@ -1,2 +1,76 @@
 #include "property_list.h"
 
+namespace sgf
+{
+
+PropertyList::PropertyList()
+{
+
+}
+
+PropertyList::PropertyList(std::map<Ogre::String, Ogre::StringVector>& property_list)
+{
+  this->swap(property_list);
+}
+
+PropertyList PropertyList::create(const Ogre::String& key, const Ogre::String& values, Ogre::String& delimiter /* = Ogre::String */)
+{
+  PropertyList newList;
+
+  newList.addProperty(key, values, delimiter);
+
+  return newList;
+}
+
+const Ogre::String& PropertyList::getValue(const Ogre::String& key, int index, const Ogre::String& defaultVal)
+{
+  PropertyList::iterator iter = find(key);
+
+  if (iter != end())
+  {
+    Ogre::StringVector values = iter->second;
+
+    if (index < static_cast<int>(values.size()))
+    {
+      return values.at(index);
+    }
+  }
+
+  return defaultVal;
+}
+
+PropertyList& PropertyList::addProperty(const Ogre::String& key, const Ogre::String& values, Ogre::String& delimiter /* = Ogre::String */)
+{
+  Ogre::StringVector result;
+  int num_splits = 0;
+  size_t start, pos;
+
+  start = 0;
+  do
+  {
+    pos = values.find_first_of(delimiter, start);
+    if (pos == start)
+    {
+      start = pos + 1; //do nothing
+    }
+    else if (pos == Ogre::String::npos)
+    {
+      result.push_back(values.substr(start)); //just copy the rest and advance
+      break;
+    }
+    else
+    {
+      result.push_back(values.substr(start, pos - start)); //copy up to next delimiter
+      start = pos + 1;
+    }
+
+    start = values.find_first_not_of(delimiter, start); //parse up to next real data
+    ++num_splits;
+  } while (pos != Ogre::String::npos);
+
+  //insert into list
+  insert(PropertyPair(key, result));
+  return *this;
+}
+
+} //sgf
--- sgf/property_system/property_list.h
+++ sgf/property_system/property_list.h
@@ -2,7 +2,30 @@
 #define _PROPERTY_LIST_H_
 
 #include <OgreString.h>
+#include <OgreStringVector.h>
 
+namespace sgf
+{
+  typedef std::pair<Ogre::String, Ogre::StringVector>   PropertyPair;
 
+  class PropertyList : public std::map<Ogre::String, Ogre::StringVector>
+  {
+  public:
+    PropertyList();
+    PropertyList(std::map<Ogre::String, Ogre::StringVector>& property_list);
+    virtual ~PropertyList() { }
+
+    static PropertyList create(const Ogre::String& key, const Ogre::String& values, Ogre::String& delimiter = Ogre::String(" "));
+
+    const Ogre::String&  getValue(const Ogre::String& key, int index, const Ogre::String& defaultVal);
+
+    PropertyList& addProperty(const Ogre::String& key, const Ogre::String& values, Ogre::String& delimiter = Ogre::String(" "));  //with support for chaining property values
+    PropertyList& operator()(const Ogre::String& key, const Ogre::String& values, Ogre::String& delimiter = Ogre::String(" "))
+    {
+      return addProperty(key, values, delimiter);
+    }
+  };
+
+} //sgf
 
 #endif
